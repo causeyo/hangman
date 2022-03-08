d21134d816cfe0dcf3c5c01abc10ebec6c887aa0
causeyo <wifonik@gmail.com>
2022-03-08 22:26:30 +0100

    update hangman game

--- main.py
+++ main.py
@@ -8,12 +8,19 @@ win=pygame.display.set_mode((winWidth,winHeight))
 
 # GLOBAL VARIABLES
 
-BLACK = (0,0, 0)
-WHITE = (255,255,255)
-RED = (255,0, 0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
-LIGHT_BLUE = (102,255,255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255,255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+LIGHT_BLUE = (102, 255, 255)
+
+btn_font = pygame.font.SysFont("arial", 20)
+guess_font = pygame.font.SysFont("monospace", 24)
+lost_font = pygame.font.SysFont('arial', 45)
+word = ''
+buttons = []
+guessed = []
 
 win.fill(GREEN)
 pygame.display.update()
